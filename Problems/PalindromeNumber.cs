05dad6b72ffda16d78a29aa23e1c0b1ef0565a68
Виктория Лейбуш <viktoriia.leibush@monopoly.su>
2022-06-03 15:25:44 +0300

    update PalindromeNumber

--- Problems/PalindromeNumber.cs
+++ Problems/PalindromeNumber.cs
@@ -4,19 +4,15 @@
     {
         public bool IsPalindrome(int x) {
             string str1 = x.ToString();
-            char[] arr = str1.ToCharArray(0, str1.Length);
-
             string str2 = "";
 
-            bool result = false;
-
             for (int i = str1.Length - 1; i >= 0; i--)
-                str2 = str2 + arr[i];
+                str2 = str2 + str1[i];
 
             if (str1 == str2)
-                result = true;
+                return true;
 
-            return result;
+            return false;
         }
     }
 }
